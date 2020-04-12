3710514d78b3691fed09ed47341562405edade00
raulgs93 <raul.garcia.santiago93@gmail.com>
2020-04-13 00:17:22 +0200

    Added music and title screen

---
+++ Assets/Scripts/PersistentObjectSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PersistentObjectSpawner : MonoBehaviour
+{
+
+    [SerializeField] GameObject persistentPrefab;
+
+    public static bool hasSpawned = false;
+
+    private void Awake() {
+        if (hasSpawned) { return; }
+
+        SpawnPersistentObjects();
+        hasSpawned = true;
+
+    }
+
+    private void SpawnPersistentObjects() {
+
+        GameObject persistentObject = Instantiate(persistentPrefab);
+        DontDestroyOnLoad(persistentObject);
+
+    }
+}
